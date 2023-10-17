e63e1f9aa93fe4eff72130eae1d8b8e70afb2093
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-17 01:35:44 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaCondicional4/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] vet = Console.ReadLine().Split(' ');
+int horaInicial = int.Parse(vet[0]);
+int horaFinal = int.Parse(vet[1]);
+
+int duracao;
+if (horaInicial < horaFinal) {
+    duracao = horaFinal - horaInicial;
+} else {
+    duracao = 24 - horaInicial + horaFinal;
+}
+
+Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
